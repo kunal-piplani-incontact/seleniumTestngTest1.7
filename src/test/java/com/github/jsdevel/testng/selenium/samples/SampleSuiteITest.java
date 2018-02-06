e0be85b0d1117b3ce5b395a3891d83e484349c48
kunal piplani <kunal.piplani@nice.com>
2018-02-06 11:23:18 +0530

    Update SampleSuiteITest.java

--- src/test/java/com/github/jsdevel/testng/selenium/samples/SampleSuiteITest.java
+++ src/test/java/com/github/jsdevel/testng/selenium/samples/SampleSuiteITest.java
@@ -21,6 +21,7 @@ public class SampleSuiteITest extends AbstractSuite<SamplePageFactory> {
 
   @Test
   public void we_should_be_able_to_pass_desired_urls_to_pages() {
+     System.out.println("Passed2");
     getPageFactory()
       .getHomePage("/intl/en/policies/privacy/?fg=1")
     ;
@@ -32,16 +33,20 @@ public class SampleSuiteITest extends AbstractSuite<SamplePageFactory> {
       .getHomePage()
       .submitSearch("dinosaurs")
     ;
+      System.out.println("Passed3");
   }
 
   @Test(expectedExceptions = PageInstantiationException.class)
   public void we_should_get_an_error_when_calling_factory_methods_with_a_return_type_that_does_not_extend_Page() {
     getPageFactory().getFoo();
+ 
+    System.out.println("e_should_get_an_error_wh");
   }
 
   @Test(expectedExceptions = PageInitializationException.class, expectedExceptionsMessageRegExp = ".+can not be viewed from.+")
   public void we_should_get_an_error_when_initializing_a_page_that_is_not_viewable_from_the_current_url() {
     getPageFactory().getSearchResultsPage("/asdfasdf");
+    System.out.println("e_should_get_an_error_whsafdf");
   }
 
   @Test @LargeDesktop
@@ -49,6 +54,7 @@ public class SampleSuiteITest extends AbstractSuite<SamplePageFactory> {
     LargeDesktop screensize = (LargeDesktop) getPageFactory().getHomePage()
         .getContext().getScreensize();
     assertNotNull(screensize);
+    System.out.println("LargeDesktop");
   }
 
   @Test
@@ -56,15 +62,20 @@ public class SampleSuiteITest extends AbstractSuite<SamplePageFactory> {
     Phone screensize = (Phone) getPageFactory().getHomePage()
         .getContext().getScreensize();
     assertNotNull(screensize);
+   System.out.println("Phone");
   }
 
   @Test @UserAgent("foo")
   public void we_should_be_able_to_set_the_user_agent_on_a_test_by_test_basis() {
     getPageFactory();
+      System.out.println("foo");
   }
 
   @Test
   public void we_should_be_able_to_configure_values_from_properties_files() {
     assertEquals("testing TestNG-Selenium", Config.LOGGING_PREFIX);
+   System.out.println("foo");
+       // assertEquals("testing ", Config.LOGGING_PREFIX);
+
   }
 }
